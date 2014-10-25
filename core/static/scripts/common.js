be1c64e3d60fe6834dcceda4d65dac09d5e622d1
Zuzanna Mroczek <z.mroczek@student.uw.edu.pl>
2014-10-25 19:07:18 +0200

    Logout

---
+++ core/static/scripts/common.js
@@ -0,0 +1,7 @@
+
+var ref = new Firebase("https://luminous-heat-6147.firebaseio.com/");
+
+var authData = ref.getAuth();
+
+document.getElementById("user-id").innerText = authData.uid;
+
--- core/static/scripts/firebase/auth.js
+++ core/static/scripts/firebase/auth.js
@@ -2,13 +2,19 @@
 var ref = new Firebase("https://luminous-heat-6147.firebaseio.com/");
 
 $(document).ready(function () {
+
     $("#login-btn").click(function () {
         ref.authWithOAuthPopup("facebook", function(error, authData) {
             if (authData) {
                 window.location.replace("/");
                 ref.child('users').child(authData.uid).set(authData);
             }
-        });;
+        });
     });
+
+    $("#logout-link").click(function () {
+        ref.unauth();
+        window.location.replace("/login");
+    })
 
 });
